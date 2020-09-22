ae283be74079c43388f15ac75747473af950ccf2
Ibrahima Barry <61597131+barrycoder123@users.noreply.github.com>
2020-09-22 15:08:09 -0400

    Hide Token

--- discord_bot.py
+++ discord_bot.py
@@ -58,4 +58,4 @@ for filename in os.listdir('./cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 
-client.run('NzMzMTA2OTQwMzMwMTE1MDgy.Xw-VCw.quDzdNOcgENd0z1J1ZRK9XYw-7A')
+client.run(TOKEN)
